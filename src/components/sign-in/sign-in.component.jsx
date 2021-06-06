7e2eefd5aec9ad7e193521295b566a73e53f755a
Cameron Lares <cameronlares@gmail.com>
2021-06-06 01:31:51 -0700

    Created handle change and handle submit method for sign in form

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,48 @@
+import { render } from '@testing-library/react';
+import React from 'react'
+ 
+import './sign-in.styles.scss'
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            passowrd: '',
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target
+        this.setState({ [name]: value})
+    }
+
+
+
+render() {
+    return (
+        <div className= 'sign-in'>
+
+<h2>I already have an account</h2>
+<span>Sign in with your email and passowrd</span>
+            <form onSubmit = {this.handleSubmit}>
+                <input name="email" type= 'email'  onChange={this.handleChange} value={this.state.email} required />
+                <label>Email</label>
+                <input name="password" type ="password" value = {this.state.password} onChange={this.handleChange}></input>
+                <label>Email</label>
+                <input type = "submit" value = "Submit Form" />
+
+            </form>
+        </div>
+    )
+}
+}
+
+export default SignIn
